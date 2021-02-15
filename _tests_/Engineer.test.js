d7b7c94819b32e1b9f481ba4212f8ed1a1ad5814
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-02-14 23:41:51 -0500

    complete test files

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,8 +1,13 @@
+const { expect } = require('@jest/globals');
 const Engineer = require('../lib/Engineer.js');
 
 test('creates an engineer object', () => {
-    const engineer = new Engineer('Engineer_1', 'Eng_1_repo');
+    const engineer = new Engineer('Engineer_1','id#1','email', 'Eng_1_repo');
 
     expect(engineer.name).toBe('Engineer_1');
-    expect(engineer.gitHubRepoName).toBe('Eng_1_repo');
+    expect(engineer.id).toBe('id#1');
+    expect(engineer.email).toBe('email');
+    expect(engineer.github).toBe('Eng_1_repo');
+    expect(engineer.role).toEqual(expect.stringContaining("Engineer"));
+
 });
--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,8 +1,12 @@
 const Intern = require('../lib/Intern.js');
 
 test('creates an intern object', () => {
-    const intern = new Intern('Intern_1', 'UofT');
+    const intern = new Intern('Intern_1', 'id#1', 'email', 'UofT');
 
     expect(intern.name).toBe('Intern_1');
+    expect(intern.id).toBe('id#1');
+    expect(intern.email).toBe('email');
     expect(intern.school).toBe('UofT');
+    expect(intern.role).toEqual(expect.stringContaining("Intern"));
+
 });
