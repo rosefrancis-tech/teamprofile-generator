22a389633017c61ba1e5157fb645819d20fb514e
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-02-14 23:27:51 -0500

    create style sheet

--- src/page-template.js
+++ src/page-template.js
@@ -3,14 +3,13 @@ const cards = employeedata => {
   ${employeedata.map(employee => {
     let optional = '';
     let icon = ''
-    //console.log(employee.role);
     switch (employee.role) {
       case 'Manager':
         optional = "Office Number: " + employee.officeNumber;
         icon = 'fas fa-mug-hot';
         break;
       case 'Engineer':
-        optional = "Github: " + employee.github;
+        optional = 'Github: <a href="https://github.com/' + employee.github + '"target="_blank">' + employee.github + '</a>'
         icon = 'fas fa-glasses';
         break;
       case 'Intern':
@@ -21,7 +20,7 @@ const cards = employeedata => {
         break;
     }
     return `
-      <div class = "col-sm-4 role">
+      <div class = "col-sm-4 newcard">
       <div class="card" style="width: 19rem;">
         <div class="card-body">
             <h4 class="card-title">${employee.name}<br /><br /><i class='${icon}'></i> ${employee.role}</h4>
@@ -48,7 +47,7 @@ module.exports = employeedata => {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Teams</title>
+        <title>Team Profile</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
         <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
@@ -59,23 +58,21 @@ module.exports = employeedata => {
           <div class="container flex-row justify-space-between align-center py-3">
             <h1 class="page-title text-secondary bg-dark py-2 px-3">Team Profile</h1>
             <nav class="flex-row">
-              <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="">Company Name</a>
+              <div class="ml-2 my-1 px-2 py-1 bg-secondary text-light" href="">${employeedata[0].teamName}</div>
             </nav>
           </div>
         </header>
         
         <main class='container'>
-          <div class= "content-wrapper row"> 
+          <div class='content-wrapper row'> 
               ${cards(employeedata)}
           </div>
         </main>
 
-        <footer class="container text-center py-3">
+        <footer class="container text-right py-3">
           <h3 class="text-dark">&copy; ${new Date().getFullYear()} by Rose</h3>
         </footer>
-      </body>
-        
-      
+      </body>   
     </html>
     `;
   };
